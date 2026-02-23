fb127b4b98010b2cab682d8d1bd1029be363d75e
Spdrd <juandiegoreyesrodriguez@gmail.com>
2026-02-23 07:38:22 -0500

    tested service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const enviroment = {
-    chatbotApiUrl: 'http://127.0.0.1:8001/api/v1/chatbot',
+    chatbotApiUrl: 'https://marckplussopenai.pocksite.com/api/v1/chatbot',
 
-    domusApiUrl: 'http://127.0.0.1:8000/api/v1/domus',
+    domusApiUrl: 'https://marckplussdomus.pocksite.com/api/v1/domus',
 }
