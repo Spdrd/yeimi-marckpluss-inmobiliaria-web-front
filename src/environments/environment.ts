a48c484e7a76a01a7fb015341191ba437aeefa14
Spdrd <juandiegoreyesrodriguez@gmail.com>
2026-04-14 14:46:33 -0500

    fix domus 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
     chatbotApiUrl: 'https://marckplussopenai.pocksite.com/api/v1/chatbot',
 
-    domusApiUrl: 'https://marckplussdomus.pocksite.com/api/v1/domus',
+    domusApiUrl: 'http://127.0.0.1:8000/api/v1/domus',
 
     whatsappNumber: '573204795284',
 
